07d0ffb014558048ffe32567abdecbe54502057f
geilsonfreire <geilosnfreireleite@gmail.com>
2026-02-23 01:11:41 -0300

    Foi implementado efeito de scrol-suave e menu ativo com click ou scrol via javaScript e CSS!

--- script.js
+++ script.js
@@ -39,3 +39,38 @@ const slides = document.querySelectorAll(".slide");
   }
 
   interval = setInterval(nextSlide, 6000);
+
+  // Efeito de scroll suave para os links de navegação e menu ativo
+  document.addEventListener("DOMContentLoaded", () => {
+
+    const sections = document.querySelectorAll("main section");
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    const observer = new IntersectionObserver(entries => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                const id = entry.target.getAttribute("id");
+
+                navLinks.forEach(link => {
+                    link.classList.remove("active");
+
+                    if (link.getAttribute("href") === `#${id}`) {
+                        link.classList.add("active");
+                    }
+                });
+            }
+
+        });
+
+    }, {
+        threshold: 0.6
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+});
