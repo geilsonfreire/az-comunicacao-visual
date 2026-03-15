35e527ad6174a3d2b12d4e10523dc3884bb1d442
geilsonfreire <geilsonfreireleite@gmail.com>
2026-03-15 15:12:19 -0300

    Ajuste do mediaquery! na ordem 900px -> 768px -> 480px ! depois refratorar o js do menu mobile!

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,18 +1,25 @@
 const toggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu-list");
 
-toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
+/* abrir e fechar pelo botão */
 
-}                                                                                                                                                                                                                               );
+toggle.addEventListener("click", (event) => {
+    event.stopPropagation(); // evita conflito com document click
+    menu.classList.toggle("active");
+});
+
+
+/* fechar clicando fora */
 
 document.addEventListener("click", (event) => {
+
     if (!menu.classList.contains("active")) return;
 
     const clickedInsideMenu = menu.contains(event.target);
-    const clickedToggle = menuToggle.contains(event.target);
+    const clickedToggle = toggle.contains(event.target);
 
     if (!clickedInsideMenu && !clickedToggle) {
         menu.classList.remove("active");
     }
+
 });
